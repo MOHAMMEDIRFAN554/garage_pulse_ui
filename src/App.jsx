b2f462947647145868b6246e465f140d13ac48d6
Bhavana V <bhavanabhavuzz03@gmail.com>
2025-10-17 13:09:24 +0530

    modified app

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,12 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const url = "http://localhost:4500/home";
-  const [message, setMessage] = useState('Loading...');
-
-  const fetchData = () => {
-    axios.get(url)
-      .then((result) => {
-        setMessage(result.data);
-      })
-      .catch((err) => {
-        console.error("Error fetching data:", err);
-        setMessage('Error connecting to backend');
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  
 
   return (
-    <div className="app-container">
-      <h1 className="title">React + Express Connection Test</h1>
-      <div className="message-box">
-        <p>{message}</p>
-      </div>
-    </div>
-  );
+    <>
+    garage pulse
+    </>)
 }
 
 export default App;
