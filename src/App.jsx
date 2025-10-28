f176931b1141b0bcb3a59a5b74601cdbc6270428
MOHAMMEDIRFAN554 <98808506+MOHAMMEDIRFAN554@users.noreply.github.com>
2025-10-29 00:04:19 +0530

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from './component/Login/authContext';
-import ProtectedRoute from './Routes/ProtectedRoute';
-import PublicRoute from './Routes/PublicRoute';
+import { AuthProvider } from "./component/Login/authContext";
+import ProtectedRoute from "./Routes/ProtectedRoute";
+import PublicRoute from "./Routes/PublicRoute";
 
-import Landing from './component/landing/Landing';
-import Registration from './component/Registration/registration';
-import Login from './component/Login/Login';
-import Home from './component/Dashboard/home/home';
+import Landing from "./component/landing/Landing";
+import Registration from "./component/Registration/registration";
+import Login from "./component/Login/Login";
+import Home from "./component/Dashboard/home/home";
 import Dashboard from "./component/Dashboard/home/Dashboard";
 import ForgotPassword from "./component/ForgotPassword/ForgotPassword";
 import NotFound from "./component/NotFoundPage/NotFound";
 import AddVehicle from "./component/Dashboard/screen/addVehicle";
-import VehicleDetails from './component/Dashboard/screen/VehicleDetails';
+import VehicleDetails from "./component/Dashboard/screen/VehicleDetails";
 import ServiceList from "./component/Service/serviceList";
 import ServiceForm from "./component/Service/serviceForm";
+
 
 function App() {
   return (
@@ -24,36 +25,48 @@ function App() {
           <Route path="/" element={<Navigate to="/landing" replace />} />
 
           {/* Public routes */}
-          <Route path="/landing" element={
-            <PublicRoute>
-              <Landing />
-            </PublicRoute>
-          } />
-
-          <Route path="/login" element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          } />
-
-          <Route path="/registration" element={
-            <PublicRoute>
-              <Registration />
-            </PublicRoute>
-          } />
-
-          <Route path="/forgot-password" element={
-            <PublicRoute>
-              <ForgotPassword />
-            </PublicRoute>
-          } />
+          <Route
+            path="/landing"
+            element={
+              <PublicRoute>
+                <Landing />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              <PublicRoute>
+                <Registration />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/forgot-password"
+            element={
+              <PublicRoute>
+                <ForgotPassword />
+              </PublicRoute>
+            }
+          />
 
           {/* Protected routes */}
-          <Route path="/home" element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route
             path="/addVehicle"
             element={
@@ -62,8 +75,14 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="/vehicle/:id" element={<ProtectedRoute><VehicleDetails /></ProtectedRoute>} />
-
+          <Route
+            path="/vehicle/:id"
+            element={
+              <ProtectedRoute>
+                <VehicleDetails />
+              </ProtectedRoute>
+            }
+          />
           <Route
             path="/ServiceList"
             element={
@@ -88,25 +107,20 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-
-          {/* 404 page */}
+          {/* 404 Page */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      <Router>
-        <Routes>
-
-        </Routes>
-      </Router>
       </Router>
     </AuthProvider>
-    
   );
 }
 
