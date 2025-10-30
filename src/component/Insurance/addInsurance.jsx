9ec74c84956f1b2dd6f45c5f16c33c467270eb09
MOHAMMEDIRFAN554 <98808506+MOHAMMEDIRFAN554@users.noreply.github.com>
2025-10-31 00:09:31 +0530

    fix

--- src/component/Insurance/addInsurance.jsx
+++ src/component/Insurance/addInsurance.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./AddInsurance.css"; 
+import "./addInsurance.css"; 
 
 const AddInsurance = () => {
   const [vehicles, setVehicles] = useState([]);
